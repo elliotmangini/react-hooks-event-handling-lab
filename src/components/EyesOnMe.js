a0fb23ddb5a4269901a1b3c4daf0fd4f9164041d
Elliot Mangini <elliot.mangini@gmail.com>
2022-08-30 09:56:46 -0600

    this lab was really fun

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,15 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+
+export default function EyesOnMe() {
+
+    const focusing = () => console.log('Good!');
+    const lookAtMe = () => console.log('Hey! Eyes on me!');
+
+    return (
+        <React.Fragment>
+            <button onFocus={focusing} onBlur={lookAtMe}>Eyes on me</button>
+        </React.Fragment>
+    )
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,22 @@
 // Code Keypad Component Here
 
+import React from 'react';
+
 function Keypad (){
+
+    function logChanging (e) {
+        e.preventDefault();
+        // e.target.reset();
+        console.log('Entering password...');
+    }
+
+
     return (
-        <div></div>
+        <div>
+            <form onChange={logChanging}>
+                <input type="password"></input>
+            </form>
+        </div>
     )
 }
 
